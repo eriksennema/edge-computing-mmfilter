6957d45709ad566e7b88f22627095ca43acb2be4
xjmxmt <42826825+xjmxmt@users.noreply.github.com>
2021-03-19 09:06:44 +0100

    Delete PretrainedModels.py

--- PretrainedModels.py
+++
@@ -1,8 +0,0 @@
-from FeatureExtractor import FeatureExtractor
-from WordEncoder import WordEncoder
-
-
-class PretrainedModels():
-    def __init__(self):
-        self.featureExtractor = FeatureExtractor()
-        self.wordEncoder = WordEncoder()
